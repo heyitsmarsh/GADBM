fd0055b34edfce210c383ba0f6598585904d0d9f
heyitsmarsh <mcalpinmarsh@aol.com>
2014-06-12 15:52:31 -0500

    Player1
    player set up with controls

---
+++ Assets/player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour
+{
+
+	public Vector2 jumpForce = new Vector2(0, 300);
+	
+
+	void Update ()
+	{
+	
+		if (Input.GetKeyUp("space"))
+		{
+			rigidbody2D.velocity = Vector2.zero;
+			rigidbody2D.AddForce(jumpForce);
+		}
+	}
+}
