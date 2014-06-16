92093327cba8a863c3e4fbe66c0af765a6290a68
AndrewSmithAie <badandyssj@gmail.com>
2014-06-16 10:10:10 -0500

    Gametime!
    Go!

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,16 +4,40 @@ using System.Collections;
 public class player : MonoBehaviour
 {
 
-	public Vector2 jumpForce = new Vector2(0, 300);
-	
+	public Vector3 jumpForce = new Vector3(0, 300);
+	public float speed = 1;
+
 
 	void Update ()
 	{
-	
+
+
+		//rigidbody.velocity.x = 1;
+		Vector3 b = new Vector3(speed,0,0);
+
+		rigidbody.AddForce(b);
 		if (Input.GetKeyUp("space"))
 		{
-			rigidbody2D.velocity = Vector2.zero;
-			rigidbody2D.AddForce(jumpForce);
+			//rigidbody.velocity = Vector3.zero;
+			rigidbody.AddForce(jumpForce);
 		}
+	
+	/*Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+	if (screenPosition.y > Screen.height || screenPosition.y < 0)
+	{
+		Die();
+	}*/
 	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		Die();
+	}
+	
+	void Die()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
 }
+
